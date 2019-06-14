daacf2ccb15ec2a1f00e3b5bd012160b1369cf24
Nicholas Rodriguez <niko876@hotmail.com>
2019-06-14 15:26:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,15 +11,15 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    answer = ''
+    phrase = ''
 
     params[:number].to_i.times do
-      answer += params[:phrase]
+      phrase += params[:phrase]
     end
 
-    answer
+    phrase
   end
-  
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
